ed649d77a874ae62f1de7b446f6fdff0df2cd089
Krzyhu <jkrzyszczuk@student.agh.edu.pl>
2024-11-25 20:14:53 +0100

    jd

--- UIX/main.py
+++ UIX/main.py
@@ -3,7 +3,6 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import StringProperty
 from kivy.lang import Builder
 
-# Wczytaj pliki .kv
 Builder.load_file("main_screen.kv")
 Builder.load_file("transcription_screen.kv")
 Builder.load_file("settings_screen.kv")
@@ -19,13 +18,11 @@ class TranscriptionScreen(Screen):
 
 class SettingsScreen(Screen):
     def save_settings(self):
-        # Pobranie ustawień od użytkownika
         selected_language = self.ids.language_spinner.text
         selected_format = self.ids.format_spinner.text
         max_space = self.ids.max_space_input.text
         selected_quality = self.ids.quality_spinner.text
 
-        # Wydruk ustawień w konsoli (można dodać logikę zapisu)
         print(f"Wybrano język: {selected_language}")
         print(f"Wybrany format zapisu: {selected_format}")
         print(f"Maksymalne miejsce: {max_space} MB")
@@ -34,7 +31,6 @@ class SettingsScreen(Screen):
 
 class MeetNotesApp(App):
     def build(self):
-        # Inicjalizacja managera ekranów
         sm = ScreenManager()
         sm.add_widget(MainScreen(name='main'))
         sm.add_widget(TranscriptionScreen(name='transcription'))
